5a6b6dfa7bdaec1b5be9cfd403254c57948012f2
Jealyang <jaelyangchoi@gmail.com>
2021-05-06 13:10:42 +0900

    자동 빈 등록, 수동 빈 등록 간의 중복 등록, 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -16,4 +19,8 @@ excludeFilters 를 이용해서 설정정보는 컴포넌트 스캔 대상에서
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    @Bean("memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
