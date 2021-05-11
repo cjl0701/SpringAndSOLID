ef7b73f30cd98b4e4000c62c8b78bc9cd83b1890
Jealyang <jaelyangchoi@gmail.com>
2021-05-11 22:49:23 +0900

    SpringBootTest에서 필드 주입 사용하기

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,17 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
+    @Autowired
+    MemberRepository memberRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
