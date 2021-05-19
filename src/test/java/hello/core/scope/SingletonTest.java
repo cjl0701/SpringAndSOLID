54f669b173d68019dd329b00356ff288d8f1c657
Jealyang <jaelyangchoi@gmail.com>
2021-05-19 22:09:15 +0900

    프로토타입 테스트

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,34 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close();
+    }
+
+    @Scope("singleton")
+    //@Conponent 없어도 ac에 직접 등록하면 빈으로 등록된다
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
